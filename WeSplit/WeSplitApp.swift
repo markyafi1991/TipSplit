ae66fd110a0ca416a14fa677c158ef7414dec9cb
Mark Yafi <mark.yafi@outlook.com>
2026-02-12 16:53:21 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mark on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
